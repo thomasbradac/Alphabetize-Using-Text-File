3de8557f9572073811ffb445c88a2bc1b1be99a2
thomas.bradac <thomas.bradac@MU-C1-10.mullen.pvt.k12.co.us>
2026-01-26 14:57:27 -0700

    Initial Commit

---
+++ Alphabetize Names/src/Alphabetize.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+public class Alphabetize
+	{
+
+		public static void main(String[] args) throws IOException
+			{
+				Scanner myFile = new Scanner(new File("Untitled 1.txt"));
+			}
+
+	}
